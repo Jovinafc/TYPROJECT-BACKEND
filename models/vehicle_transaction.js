3e6ef11d7716f60bcfa74dc730e50dfc60e0c826
unknown <dsouza.jovin65@gmail.com>
2019-03-31 23:27:30 +0530

    booking cancelled sorted

---
+++ models/vehicle_transaction.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const vehicle_transaction = sequelize.define('vehicle_transaction', {
+      id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: DataTypes.INTEGER
+      },
+    client_id: DataTypes.BIGINT,
+    owner_id: {
+         type: DataTypes.BIGINT,
+        references:{
+            model:"owners",
+            key:"owner_id"
+
+        }
+    },
+    vehicle_id:{
+      type:DataTypes.BIGINT,
+        references:{
+          model:"vehicle_transaction",
+          key:"vehicle_id"
+        }
+
+    } ,
+    user_id:DataTypes.BIGINT,
+    transaction_type: DataTypes.STRING,
+    from: DataTypes.STRING,
+    to: DataTypes.STRING,
+      amount:DataTypes.DOUBLE,
+    status: DataTypes.STRING
+  }, {});
+  vehicle_transaction.associate = function(models) {
+      vehicle_transaction.belongsTo(models.owner,{foreignKey:"owner_id"})
+      vehicle_transaction.belongsTo(models.vehicle,{foreignKey: "vehicle_id"})
+       vehicle_transaction.hasOne(models.rating,{foreignKey:"vehicle_id"})
+      vehicle_transaction.hasOne(models.feedback,{foreignKey:"vehicle_id"})
+      vehicle_transaction.hasMany(models.rent,{foreignKey:"vehicle_id"})
+  };
+  return vehicle_transaction;
+};
